f2ef830372a5e70e8905d123abded81114a155a6
dev <dev@localhost>
2021-08-01 16:04:16 +0100

    A new sample program on 1st of August,2021

---
+++ src/com/tcs/SeperateExample.java
@@ -0,0 +1,38 @@
+package com.tcs;
+
+public class SeperateExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* int a=200;
+		float f=a;
+		System.out.println(a);
+		
+		System.out.println(f); */
+		
+		System.out.println("Program swapping of 2 no's:\n ");
+		
+		System.out.println("Before swapping: ");
+
+		
+		double b = 13.445;
+		double c = 24.5;
+		
+		System.out.println(b);
+		System.out.println(c);
+		
+		System.out.println("\nAfter swapping: ");
+		
+		double temp = b;
+		b = c;
+		c = temp;
+		
+		System.out.println(b);
+		System.out.println(c);
+
+		
+
+	}
+
+}
